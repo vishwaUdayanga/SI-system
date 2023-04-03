6d9d07bac2fb5cc2001c3a9729ed3e28a3df81e5
Vishwa Udayanga <127868013+vishwaUdayanga@users.noreply.github.com>
2023-04-03 19:31:36 +0530

    Update CourseDetails.js

--- src/inner-components/student-courses/CourseDetails.js
+++ src/inner-components/student-courses/CourseDetails.js
@@ -19,7 +19,7 @@ function CourseDetails() {
                     <p>Thursday -- 10.45 a.m. - 12.45 p.m.</p>
                     <p>12th Floor Auditorium</p>
                     <h5>Enrollment Key - PSinC2023</h5>
-                    <p>Anurudda Abesinghe - <Link className="emails">anuruddha.a@sliit.lk</Link></p>
+                    <p>Anurudda Abesinghe - <Link  to='#' onClick={(e) => {window.location.href = 'mailto:anuruddha.a@sliit.lk'; e.preventDefault();}} className="emails">anuruddha.a@sliit.lk</Link></p>
                 </div>
                 <div className='course-card'>
                     <h4>Foundation of Mathematical Skills</h4>
@@ -27,7 +27,7 @@ function CourseDetails() {
                     <p>Thursday -- 10.45 p.m. - 12.45 p.m.</p>
                     <p>12th Floor Auditorium</p>
                     <h5>Enrollment Key - FMS@23</h5>
-                    <p>Chamith Jayasinghe - <Link className="emails">chamith.j@sliit.lk</Link></p>
+                    <p>Chamith Jayasinghe - <Link  to='#' onClick={(e) => {window.location.href = 'mailto:chamith.j@sliit.lk'; e.preventDefault();}} className="emails">chamith.j@sliit.lk</Link></p>
                 </div>
                 <div className='course-card'>
                     <h4>Programming Design Techniques</h4>
@@ -35,7 +35,7 @@ function CourseDetails() {
                     <p>Thursday -- 8.30 a.m. - 10.30 a.m.</p>
                     <p>12th Floor Auditorium</p>
                     <h5>Enrollment Key - IT1005</h5>
-                    <p>Rushira ManikkaArachchi - <Link className="emails">ruchira.m@sliit.lk</Link></p>  
+                    <p>Rushira ManikkaArachchi - <Link  to='#' onClick={(e) => {window.location.href = 'mailto:ruchira.m@sliit.lk'; e.preventDefault();}} className="emails">ruchira.m@sliit.lk</Link></p>  
                 </div>
             </div>
         </div>
